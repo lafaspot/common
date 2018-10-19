a3e1a0ac94dd245cbbff294ceb33578d0f13e4b2
jui8wang <jui8wang@gmail.com>
2018-10-19 13:47:26 -0700

    Adding WorkerModel and test.

---
+++ core/src/main/java/com/lafaspot/common/concurrent/WorkerModel.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright [yyyy] [name of copyright owner]
+ * 
+ * ====================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *  ====================================================================
+ */
+package com.lafaspot.common.concurrent;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Identifies classes that implements worker model.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface WorkerModel {
+    /**
+     * Defines how the worker operations in complete async model or sync/blocking.
+     */
+    enum Model {
+        /** Worker that runs on async pool. */
+        ASYNC,
+        /** Worker that runs on sync pool. */
+        SYNC
+    }
+
+    /**
+     * The type, Sync or Async.
+     */
+    Model type();
+
+    /**
+     * Description of the worker.
+     */
+    String description();
+}
